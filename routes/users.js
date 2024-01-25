06b073034ca36ff91bb5403a5a3cd574dc7fe588
Ashwan1-yadav <ashwaniyadav888@gmail.com>
2024-01-25 18:00:53 +0530

    Instagram Clone

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/Instagram");
+const userSchema = mongoose.Schema({
+  username : String,
+  name : String,
+  password : String,
+  email : String,
+  profileImage : String,
+  bio : String, 
+  posts : [{
+    type : mongoose.Schema.Types.ObjectId, ref : "post"
+  }],
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
